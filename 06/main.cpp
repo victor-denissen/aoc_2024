4bfbb3b45cdfc179458b882f49f81f19b20e6a63
Victor Denissen <vdenisse@a1-r2-p1.s19.be>
2024-12-23 16:52:40 +0100

    day 6 part 2 , i think it works but it takes a little while

--- 06/main.cpp
+++ 06/main.cpp
@@ -9,6 +9,14 @@
 #include <stdlib.h>
 #include "../colors.h"
 using namespace std;
+
+void	freeArray(char ** map, int size)
+{
+	for (int i = 0; i < size; i++)
+		free(map[i]);
+	free(map);
+}
+
 char ** readFile(int * size, string file)
 {
 	ifstream input;
@@ -26,10 +34,9 @@ char ** readFile(int * size, string file)
 	for (string str : vec_result)
 	{
 		result[i] = strdup(str.c_str());
-		if (!result[i]) {
-			while (i >= 0)
-				free(result[i--]);
-			free(result);
+		if (!result[i]) 
+		{
+			freeArray(result, i-1);
 			return NULL;
 		}
 		i++;
@@ -40,9 +47,10 @@ char ** readFile(int * size, string file)
 
 void	findGuard(char ** map, pair<int, int> & coords, int size)
 {
+	string guards = "<>^v";
 	for (int i = 0; i < size; i++)
 		for (int j = 0; j < strlen(map[i]); j++)
-			if (map[i][j] == '^')
+			if (guards.find(map[i][j]) != string::npos)
 				coords = {i,j};
 }
 
@@ -67,10 +75,12 @@ int	moveGuard(char ** map, pair<int, int> & coords, int size)
 			throw runtime_error("what is my guard");
 	}
 	pair<int, int> newCoords = {coords.first + move.first, coords.second + move.second};
-	if (		newCoords.second == strlen(map[newCoords.second]) ||	// Guard moves of right side 
-			newCoords.second < 0 ||					// Guard moves of left side
+	if (		
 			newCoords.first == size ||				// Guard moves of bottom side
-			newCoords.first < 0)					// Guard moves of top side
+			newCoords.first < 0	||					// Guard moves of top side
+			newCoords.second == strlen(map[newCoords.first]) ||	// Guard moves of right side 
+			newCoords.second < 0					// Guard moves of left side
+			)
 	{
 		map[coords.first][coords.second] = 'X'; // Mark map for final time
 		return 1;
@@ -144,10 +154,102 @@ void	part1(string file = "input")
 	cout << BRIGHT_GREEN << result << RESET << endl;
 }
 
+char **	copyMap(char **map, int size)
+{
+	char **	copy = (char **)malloc(size * sizeof(char *));
+	if (!copy)
+		return NULL;
+	for (int i = 0; i < size; i++)
+	{
+		copy[i] = strdup(map[i]);
+		if (!copy[i])
+		{
+			freeArray(copy, i-1);
+			return NULL;
+		}
+	}
+	return copy;
+}
+
+pair<pair<int, int>, char> createPosition(char ** map, int size)
+{
+	pair<int, int> newCoords;
+	findGuard(map, newCoords, size);
+	char c = map[newCoords.first][newCoords.second];
+	pair<pair<int, int>, char> newPos(newCoords, c);
+	return newPos;
+}
+
+bool	checkDuplicatePosition(vector<pair<pair<int, int>, char>> positions, pair<pair<int, int>, char> newPos)
+{
+	for (pair<pair<int, int>, char> p: positions)
+		if (p == newPos)
+			return true;
+	return false;
+}
+
+int	checkIfLoop(char ** map, int size, pair<int, int> guard)
+{
+	vector<pair<pair<int, int>, char>> positions;
+	auto newPos = createPosition(map, size);
+	positions.push_back(newPos);
+	while (!moveGuard(map, guard , size))
+	{
+		newPos = createPosition(map, size);
+		if (checkDuplicatePosition(positions, newPos))
+		{
+			printMap(map, size);
+			return 1;
+		}
+		positions.push_back(newPos);
+	}
+	return 0;
+}
+
+int	part2solution(char ** map, int size, pair<int,int> guard)
+{
+	int result = 0;
+	pair<int, int> obs(0, 0);
+	for (int i = 0; i < size; i++)
+	{
+		obs.second = 0;
+		for (int j = 0; j < strlen(map[i]); j++, obs.second++)
+		{
+			char ** copy = copyMap(map, size);
+			if (copy[obs.first][obs.second] == '.')
+				copy[obs.first][obs.second] = '#';
+			else
+			{
+				freeArray(copy, size);
+				continue ;
+			}
+			cout << "Testing: " << obs.first << ", " << obs.second << endl;
+			result += checkIfLoop(copy, size, guard);
+			freeArray(copy, size);
+		}
+		obs.first++;
+	}
+	return (result);
+}
+
+void	part2(string file = "input")
+{
+	int size;
+	char ** map = readFile(&size, file);
+	if (!map)
+		return ;
+	pair<int, int> guard(-1, -1);
+	findGuard(map, guard, size);
+	if (guard.first == -1)
+		return (void(cout << RED << "No guard found" << endl << RESET));
+
+	cout << BRIGHT_GREEN << part2solution(map, size, guard) << RESET << endl;
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
-		part1(argv[1]);
+		part2(argv[1]);
 	else
 		part1();
 	return (0);
